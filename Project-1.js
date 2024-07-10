55b8afe810c2a077b1a6fbdf4f063cc6f202bdc7
mbaker <mekenna.drabner@gmail.com>
2024-07-10 01:20:08 -0600

    styled navbar elements. positioned cards. placed text in footer

--- Project-1.js
+++ Project-1.js
@@ -8,32 +8,32 @@ const url = `https://api.api-ninjas.com/v1/geocoding?city=${City}&state=${State}
 async function getCoordinates() {
     const options = {
         headers: {
-            'X-Api-Key': 'bqCDMIQBl8n8JAdyO7tMYw==cmnAhPj015c3slkP' 
+            'X-Api-Key': 'bqCDMIQBl8n8JAdyO7tMYw==cmnAhPj015c3slkP'
         }
     };
-    
+
     const result = await fetch(url, options);
-    
+
     if (!result.ok) {
         throw new Error('Network response was not ok ' + result.statusText);
     }
 
     const data = await result.json();
-  
+
 
     const weatherUrl = `https://api.api-ninjas.com/v1/weather?lat=${data[0].latitude}&lon=${data[0].longitude}`;
-            
-            const weatherResponse = await fetch(weatherUrl, options);
-            
-            if (!weatherResponse.ok) {
-                throw new Error('Network response was not ok ' + weatherResponse.statusText);
-            }
 
-            const weatherData = await weatherResponse.json();
-            console.log(weatherData);
-    
+    const weatherResponse = await fetch(weatherUrl, options);
 
-    
+    if (!weatherResponse.ok) {
+        throw new Error('Network response was not ok ' + weatherResponse.statusText);
+    }
+
+    const weatherData = await weatherResponse.json();
+    console.log(weatherData);
+
+
+
 }
 
 getCoordinates();
